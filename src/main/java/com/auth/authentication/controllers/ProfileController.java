749a7127ff25e38dc86adfc9fb0fcf2f6e79b293
Matthew Greco <mgreco2112@tutanota.com>
2022-04-01 22:16:16 -0400

    finished create method

--- src/main/java/com/auth/authentication/controllers/ProfileController.java
+++ src/main/java/com/auth/authentication/controllers/ProfileController.java
@@ -1,12 +1,15 @@
 package com.auth.authentication.controllers;
 
+import com.auth.authentication.auth.User;
 import com.auth.authentication.models.Profile;
 import com.auth.authentication.repositories.ProfileRepository;
 import com.auth.authentication.repositories.UserRepository;
 import com.auth.authentication.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.DefaultEventListenerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -21,6 +24,8 @@ public class ProfileController {
     UserRepository userRepository;
     @Autowired
     ProfileRepository repository;
+    @Autowired
+    UserService userService;
 
     @GetMapping
     public List<Profile> getAllProfiles() {
@@ -58,7 +63,23 @@ public class ProfileController {
 
     @PostMapping
     public ResponseEntity<Profile> createNewProfile(@RequestBody Profile profile) {
+        User currentUser = userService.getCurrentUser();
 
+        if (currentUser == null) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
+        for (Profile selProfile : repository.findAll()) {
+            if (selProfile.getUser() == currentUser) {
+                return  new ResponseEntity<>(null, HttpStatus.CONFLICT);
+            }
+        }
+
+        profile.setUser(currentUser);
+
+        Profile newProfile = repository.save(profile);
+
+        return new ResponseEntity<>(newProfile, HttpStatus.CREATED);
     }
 
 
