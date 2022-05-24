b41a86cddb3cb3ae7474e5d09adcd8a058fa7471
Matthew Greco <mgreco2112@tutanota.com>
2022-05-24 11:20:33 -0400

    filled out Post and Post Controller with minimum features

--- src/main/java/com/auth/authentication/controllers/PostController.java
+++ src/main/java/com/auth/authentication/controllers/PostController.java
@@ -1,4 +1,66 @@
 package com.auth.authentication.controllers;
 
+import com.auth.authentication.models.Post;
+import com.auth.authentication.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/post")
 public class PostController {
+
+    @Autowired
+    private PostRepository repository;
+
+    @GetMapping
+    public List<Post> findAllPosts() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> findPostById(@PathVariable Long id) {
+        Post post = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        return ResponseEntity.ok(post);
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<Post> createNewPost(@RequestBody Post newPost) {
+        return new ResponseEntity<>(repository.save(newPost), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Post> updatePost(@RequestBody Post updates) {
+        Post selPost = repository.findById(updates.getId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (updates.getTitle() != null) {
+            selPost.setTitle(updates.getTitle());
+        }
+        if (updates.getBody() != null) {
+            selPost.setBody(updates.getBody());
+        }
+        if (updates.getTopic() != null) {
+            selPost.setTopic(updates.getTopic());
+        }
+        if (updates.getReplies() != null) {
+            selPost.setReplies(updates.getReplies());
+        }
+
+        return ResponseEntity.ok(repository.save(selPost));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deletePost(@RequestBody Post deleted) {
+        Post delPost = repository.findById(deleted.getId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        repository.delete(delPost);
+
+        return ResponseEntity.ok("Deleted Post");
+    }
 }
--- src/main/java/com/auth/authentication/models/Post.java
+++ src/main/java/com/auth/authentication/models/Post.java
@@ -1,5 +1,12 @@
 package com.auth.authentication.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Set;
+
+@Entity
 public class Post {
     /*
     fields:
@@ -9,4 +16,64 @@ public class Post {
     String topic (if null, post is head post)
     Set<Post> replies (other posts)
      */
+
+    //TODO create relationship with Collector, One Collector to Many Posts
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String body;
+    private String title;
+    private String topic;
+    private Set<Post> replies;
+
+    public Post() {
+    }
+
+    public Post(Long id, String body, String title, String topic) {
+        this.id = id;
+        this.body = body;
+        this.title = title;
+        this.topic = topic;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Set<Post> getReplies() {
+        return replies;
+    }
+
+    public void setReplies(Set<Post> replies) {
+        this.replies = replies;
+    }
 }
