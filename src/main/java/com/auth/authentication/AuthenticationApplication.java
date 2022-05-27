2526171478babf87fb417bbeac3a58e86cec5a34
TBlais10 <blaislighting@gmail.com>
2022-05-26 20:22:30 -0400

    Uncommented method in RoleRepository and created injectRole method inside of the main application class

--- src/main/java/com/auth/authentication/AuthenticationApplication.java
+++ src/main/java/com/auth/authentication/AuthenticationApplication.java
@@ -1,13 +1,68 @@
 package com.auth.authentication;
 
+import com.auth.authentication.auth.ERole;
+import com.auth.authentication.repositories.RoleRepository;
+import com.auth.authentication.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 @SpringBootApplication
 public class AuthenticationApplication {
 
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Value("${spring.datasource.driver-class-name}")
+	public String myDriver;
+
+	@Value("${spring.datasource.url}")
+	public String myUrl;
+
+	@Value("${spring.datasource.username}")
+	public String username;
+
+	@Value("${spring.datasource.password}")
+	public String password;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationApplication.class, args);
+	}
+
+	@Bean
+	public void injectRoles() {
+		int roleCheck = roleRepository.isRoleEmpty();
+
+		if (roleCheck < ERole.values().length) {
+			int id = 1;
+			for (ERole role : ERole.values()) {
+				if (roleRepository.findByName(role).isEmpty()) {
+					try {
+						Connection conn = DriverManager.getConnection(myUrl, username, password);
+						Class.forName(myDriver);
+						String query = "Insert into role (id, name) values (?,?)";
+						PreparedStatement statement = conn.prepareStatement(query);
+
+						statement.setString(1, Integer.toString(id));
+						statement.setString(2, role.toString());
+
+						statement.executeUpdate();
+					} catch (Exception e) {
+						System.out.println(e.getMessage());
+					}
+				}
+				id++;
+			}
+		}
 	}
 
 }
--- src/main/java/com/auth/authentication/repositories/RoleRepository.java
+++ src/main/java/com/auth/authentication/repositories/RoleRepository.java
@@ -3,6 +3,8 @@ package com.auth.authentication.repositories;
 import com.auth.authentication.auth.ERole;
 import com.auth.authentication.auth.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,7 +13,6 @@ import java.util.Optional;
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(ERole name);
 
-    /*
     @Query(value = "select count(*) from role",
             nativeQuery = true)
     int isRoleEmpty();
@@ -19,6 +20,5 @@ public interface RoleRepository extends JpaRepository<Role, Long> {
     @Query(value = "Insert into role (name) name values(:roleName) ",
             nativeQuery = true)
     void insertRoles(@Param("roleName") ERole role);
-     */
 
 }
